e8d08b665b6e22ff7b8974b62a87b15f117a88d4
hbehjat <hamid.behjat@gmail.com>
2022-12-02 16:44:03 +0100

    minimal version, to transfer to Python, not suitable for handling graph signals

---
+++ src/MATLAB/voxbg_get_struct_mini.m
@@ -0,0 +1,300 @@
+% Note to Mathieu: 
+%
+% [1] This function is related to building both CHC and WM voxBG. To start
+% with, just code the parts related to CHC voxBG. In the code below:
+% - CHC graph has G.tissue = 'gmlh' or 'gmrh'
+% - WM graph has G.tissue = 'wm'
+% As such, whereever there is a "Switch case .." statement, only transfer
+% the code under case {'gmlh','gmrh'}.
+% 
+% [2] Also skip parts inside the "if opts.also_get_files_related_to_fMRI"
+% statement. 
+
+function  G = voxbg_get_struct_mini(ID,gtype,opts)
+%VOXBG_GET_STRUCT creates G structure. If G exists, it is loaded and
+%verified, and if necessary, it's content is updated.
+%
+% Hamid Behjat
+
+opts.also_get_files_related_to_fMRI = false;
+
+if ~ischar(ID)
+    ID = num2str(ID);
+end
+
+hcp_root= opts.hcp_root;
+
+if ~isfield(opts,'hcpsave_root') || isempty(opts.hcpsave_root)
+    hcpsave_root = hcp_root;
+else
+    hcpsave_root = opts.hcpsave_root;
+end
+
+G = struct;
+G.type = gtype;
+G.subject = ID;
+G.fname = [];
+d = strfind(gtype,'.res');
+G.tissue = gtype(1:d-1);
+
+switch G.tissue
+    case 'gmlh'
+        G.hemisphere = 'left';
+    case 'gmrh'
+        G.hemisphere = 'right';
+    case 'wm'
+        G.hemisphere = 'both';
+end
+
+G.surftype = 'n/a';
+
+G.res = gtype(d+(4:7));
+
+if contains(gtype,'T1w')
+    G.space = 'T1w';
+elseif contains(gtype,'Diffusion')
+    G.space = 'Diffusion';
+end
+
+G.resTag   = ['.res',G.res];
+G.spaceTag = ['.space',G.space];
+G.rsTag    = [G.resTag,G.spaceTag];
+G.trsTag   = [G.tissue,G.rsTag];
+
+switch G.tissue
+    case 'wm'
+        if contains(gtype,'.fod')
+            G.diffusionModel = 'fod';
+            G.settingsTag = gtype(strfind(gtype,'.fixels'):end);
+            G.N_fixels = str2double(G.settingsTag(strfind(G.settingsTag,'.fixels')+length('.fixels'):strfind(G.settingsTag,'.maxAfd')-1));
+            if contains(G.settingsTag,'.weighted')
+                d1 = strfind(G.settingsTag,'.weighted');
+                d2 = strfind(G.settingsTag,'.onlywm');
+                G.weightingType = lower(G.settingsTag(d1+9:d2-1));
+                tag = '.weighted';
+            elseif contains(G.settingsTag,'.nonweighted')
+                G.weightingType = 'none';
+                tag = '.nonweighted';
+            else
+                error('extend..');
+            end
+            G.maxAfd = (1/100)*str2double(G.settingsTag(strfind(G.settingsTag,'.maxAfd')+length('.maxAfd'):strfind(G.settingsTag,tag)-1));
+            G.neighb = 5;
+            G.rempar = true;
+        else
+            G.diffusionModel = 'odf';
+            G.settingsTag = gtype(strfind(gtype,'.neighb'):end);
+            d = G.settingsTag(strfind(G.settingsTag,'.neighb')+length('.neighb'):strfind(G.settingsTag,'.pow')-1);
+            if length(d)==1
+                G.neighb = str2double(d);
+                G.neighbHybrid = false;
+            elseif isequal(d,'5+3')
+                G.neighb = 5;
+                G.neighbHybrid = true; % WM-WM, WM-GM: neighb5, GM-GM: neighb3
+            else
+                error('invalid neighborhood connectivity specifier');
+            end
+            G.odfPow    = str2double(G.settingsTag(strfind(G.settingsTag,'.pow')   +length('.pow')   :strfind(G.settingsTag,'.fibs')-1));
+            G.N_fibers  = str2double(G.settingsTag(strfind(G.settingsTag,'.fibs')  +length('.fibs')  :strfind(G.settingsTag,'.rempar')-1));
+            G.rempar    = str2double(G.settingsTag(strfind(G.settingsTag,'.rempar')+length('.rempar'):strfind(G.settingsTag,'.sh')-1));
+            G.shOrder   = str2double(G.settingsTag(strfind(G.settingsTag,'.sh')    +length('.sh')    :strfind(G.settingsTag,'.th')-1));
+            G.thresh    = str2double(G.settingsTag(strfind(G.settingsTag,'.th')    +length('.th')    :strfind(G.settingsTag,'.beta')-1)); % \alpha parameter in the sigmoid function
+            G.beta      = str2double(G.settingsTag(strfind(G.settingsTag,'.beta')+length('.beta'):strfind(G.settingsTag,'.mag')-1)); % \beta parameter in the sigmoid function
+            G.magnitude = str2double(G.settingsTag(strfind(G.settingsTag,'.mag')+length('.mag'))); %:d(end-1)-1)); % use magnitude term as in Anjali's work?
+            d = strfind(G.settingsTag,'.');
+            G.method = G.settingsTag(d(end-1)+1:d(end)-1);
+        end
+        switch G.tissue
+            case 'wm'
+                d = str2double(G.settingsTag(strfind(G.settingsTag,'.onlywm')+length('.onlywm'))); % only WM as graph nodes or include other subcortical stuff?
+                switch d
+                    case 1
+                        G.maskType    = 'onlyWM';
+                        G.maskTypeTag = '.onlywm1';
+                    case 0
+                        G.maskType    = 'subcortical';
+                        G.maskTypeTag = '.onlywm0';
+                end
+        end
+        G.maskTypeTag = sprintf('%s.neighb%d',G.maskTypeTag,G.neighb);
+    case {'gmlh','gmrh'}
+        G.settingsTag = '';
+        G.neighb = 3;
+        G.maskTypeTag = sprintf('.neighb%d',G.neighb);
+end
+
+G.f = [];
+f = struct;
+
+%-Directories--------------------------------------------------------------
+f.hcp_root = hcp_root;
+f.hcpsave_root = hcpsave_root;
+f.T1w = fullfile(hcp_root,ID,'T1w');
+f.MNI = fullfile(hcp_root,ID,'MNINonLinear');
+f.T1w_save         = fullfile(hcpsave_root,ID,'T1w');
+f.T1w_results_save = fullfile(f.T1w_save,'Results');
+f.MNI_save         = fullfile(hcpsave_root,ID,'MNINonLinear');
+f.MNI_results      = fullfile(f.MNI,'Results');
+f.MNI_results_save = fullfile(f.MNI_save,'Results');
+f.graphmain        = fullfile(f.T1w_save,'graph');
+f.graph            = fullfile(f.graphmain,strrep(gtype,'.','_'));
+[~,~] = mkdir(f.T1w_save); % outputs set to prevent warnings
+[~,~] = mkdir(f.MNI_save);
+[~,~] = mkdir(f.MNI_results_save);
+[~,~] = mkdir(f.graph);
+switch G.tissue
+    case 'wm'
+        f.diffusion        = fullfile(f.T1w,'Diffusion');
+        f.diffusion_save   = fullfile(f.T1w_save,'Diffusion');
+        [~,~] = mkdir(f.diffusion_save);
+end
+f.G = fullfile(f.graph,['G.',G.type,'.mat']);
+
+n = [G.tissue,G.maskTypeTag,G.resTag];
+
+%-Volumetric files---------------------------------------------------------
+switch G.tissue
+    case {'gmlh','gmrh'}
+        f.source = fullfile(f.T1w,'ribbon.nii');
+        f.mask = fullfile(f.graph,[n,'.spaceT1w.nii']);
+    case 'wm'
+        switch G.tissue
+            case 'wm'
+                f.source = fullfile(f.diffusion,'nodif_brain_mask.nii');
+                f.mask = fullfile(f.graph,[n,'.DiffusionSpace.nii']);
+                if contains(gtype,'.fod')
+                    f.mask_afdPruned = strrep(f.mask,'.nii','.afdPruned.nii');
+                    f.mask_fixelOrientationPruned = strrep(f.mask,'.nii','.fixelOrientationPruned.nii');
+                end
+            otherwise
+                
+        end
+        f.diffusion_data = fullfile(f.diffusion_save,'data.nii.gz');
+        f.t1w_graphspace = fullfile(f.graph,'t1w.DiffusionSpace.nii');
+        f.t2w_graphspace = fullfile(f.graph,'t2w.DiffusionSpace.nii');
+        f.aparcaseg_graphspace = fullfile(f.graph,'aparc+aseg.DiffusionSpace.nii');
+        f.aparcaseg = fullfile(f.T1w,'aparc+aseg.nii'); % not used in graph definition; just used to extract wm part within subcortical region used as graph mask, for debuging purposes
+        f.wm_graphspace = fullfile(f.graph,'aparc+aseg_wm.DiffusionSpace.nii');
+end
+
+%-Surface files------------------------------------------------------------
+d_surf = fullfile(hcpsave_root,ID,'T1w',ID,'surf'); %see NOTE 1.
+[~,~] = mkdir(d_surf);
+
+switch G.tissue
+    case {'gmlh','gmrh'}
+        d = G.tissue(3:4);
+        f.surface.pial = {fullfile(d_surf,[d,'.pial.surf.gii'])};
+        f.surface.white = {fullfile(d_surf,[d,'.white.surf.gii'])};
+        f.surfmesh.pial = fullfile(d_surf,[d,'.pial']);
+        f.surfmesh.white = fullfile(d_surf,[d,'.white']);
+        f.surfmesh.pial_ascii = fullfile(d_surf,[d,'.pial.asc']);
+        f.surfmesh.white_ascii = fullfile(d_surf,[d,'.white.asc']);
+    case 'wm'
+        f.surface.white = {...
+            fullfile(d_surf,'lh.white.surf.gii'),...
+            fullfile(d_surf,'rh.white.surf.gii')};
+end
+
+fn = fieldnames(f.surface);
+for iFN=1:length(fn)
+    d = f.surface.(fn{iFN});
+    for iF=1:length(d)
+        if ~exist(d{iF},'file')
+            if exist(fullfile(hcp_root,ID,'preproc'),'dir') % ldcNAS format
+                d1 = fullfile(hcp_root,ID,'preproc',ID,'T1w',ID,'surf');
+            else
+                d1 = fullfile(hcp_root,ID,'T1w',ID,'surf');
+            end
+            [~,d2,d3] = fileparts(d{iF});
+            sts = copyfile(fullfile(d1,[d2,d3]),d{iF});
+            if ~sts, error('[HB] problem copying surface file.'); end
+        end
+    end
+end
+
+
+if opts.also_get_files_related_to_fMRI
+    %-Mapping files------------------------------------------------------------
+    f.xfms = fullfile(f.MNI,'xfms');
+    f.xfms_save = fullfile(f.MNI_save,'xfms');
+    [~,~] = mkdir(f.xfms_save);
+    
+    % Displacement field for mapping from MNI to ACPC
+    f.disp_mni2acpc = fullfile(f.xfms,'standard2acpc_dc.nii');
+    
+    % Displacement field for mapping from ACPC to MNI
+    f.disp_acpc2mni = fullfile(f.xfms,'acpc_dc2standard.nii');
+    
+    
+    %-Preprocessed fMRI volumes in MNI space-------------------------------
+    % address to fMRI data that
+    % have been coregistered
+    % with graph.
+    
+    %d = load('tasks9_LR.mat','tasks');
+    %taskSets{1} = d.tasks;
+    
+    taskSets{1} = [
+        {'tfMRI_EMOTION_LR'   }
+        {'tfMRI_GAMBLING_LR'  }
+        {'tfMRI_LANGUAGE_LR'  }
+        {'tfMRI_MOTOR_LR'     }
+        {'tfMRI_RELATIONAL_LR'}
+        {'tfMRI_SOCIAL_LR'    }
+        {'tfMRI_WM_LR'        }
+        {'rfMRI_REST1_LR'     }
+        {'rfMRI_REST2_LR'     }];
+    
+    %d = load('tasks9_RL.mat','tasks');
+    %taskSets{2} = d.tasks;
+    
+    taskSets{2} = [
+        {'tfMRI_EMOTION_RL'   }
+        {'tfMRI_GAMBLING_RL'  }
+        {'tfMRI_LANGUAGE_RL'  }
+        {'tfMRI_MOTOR_RL'     }
+        {'tfMRI_RELATIONAL_RL'}
+        {'tfMRI_SOCIAL_RL'    }
+        {'tfMRI_WM_RL'        }
+        {'rfMRI_REST1_RL'     }
+        {'rfMRI_REST2_RL'     }];
+    
+    for iS=1:2
+        tasks = taskSets{iS};
+        for t = 1:length(tasks)
+            task = tasks{t};
+            n = [task '.nii'];
+            f.fmri_mni.(task)      = fullfile(f.MNI_results,task,n);               % on original volume
+            f.fmri_mni_save.(task) = fullfile(f.MNI_results_save,task,n);          % on writable volume
+        end
+    end
+    
+    %-Resliced fMRI volumes that have 1-to-1 voxel macth with G.f.mask-----
+    % address to fMRI data that have been coregistered with graph.
+    for iS=1:2
+        tasks = taskSets{iS};
+        for t = 1:length(tasks)
+            task = tasks{t};
+            n = [task,G.rsTag,'.nii'];
+            f.fmri_graph.(task) = fullfile(f.T1w_results_save,task,n);
+        end
+    end
+    
+    %-fMRI graph signals---------------------------------------------------
+    % signals extracted from graph coregistered fMRI volumes.
+    for iS=1:2
+        tasks = taskSets{iS};
+        for t = 1:length(tasks)
+            task = tasks{t};
+            n = ['G.',G.type,'.signals_',task,'.mat'];
+            f.signals.(task) = fullfile(f.graph,n);
+        end
+    end
+end
+
+G.f = f;
+
+G.fname = fullfile(f.graph,['G.',G.type,'.mat']);
+
+end
