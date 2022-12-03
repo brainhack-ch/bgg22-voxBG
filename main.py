fdc1b2119d28fc1f087c4ecd16231b6db2ed2b04
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2022-12-03 16:36:52 +0100

    fix removal of isolated voxels

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import numpy as np
 from nilearn.image import load_img, resample_img, resample_to_img, new_img_like
-from scipy.ndimage import label
+from scipy import ndimage
 
 folder_hcp = Path(
     "/Users/scheltie/git/brainhack-ch/bgg22-voxBG/demos/data/HCP/dataset/"
@@ -35,13 +35,13 @@ img_resampled = resample_img(
 # resample_to_img(source_img, target_img)
 
 # remove isolated voxels
-label, nb_obj = label(img_resampled.get_fdata())
+label, nb_obj = ndimage.label(img_resampled.get_fdata())
 # find the largest connected components
 sizes = [np.where(label.flatten() == k)[0].size for k in range(1, nb_obj + 1)]
 keep = np.argmax(sizes) + 1
 # set to 0 voxels to remove
 data = img_resampled.get_fdata()
-data[np.where(data != keep)] = 0
+data[np.where(label != keep)] = 0
 img_resampled = new_img_like(img_resampled, data=data, copy_header=True)
 
 # compute adjacency of the voxels selected and resampled
